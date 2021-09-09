c0d5aaef52d55c15ce4e8befc17f004d5f696cf4
quazzze <mvd170201@gmail.com>
2021-09-09 15:42:03 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace Launcher2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblVersion = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(768, 434);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 19);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(768, 12);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 19);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(768, 37);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 19);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "button2";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 434);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(747, 19);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.label1.Location = new System.Drawing.Point(704, 420);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 11);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.label2.Location = new System.Drawing.Point(15, 420);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 11);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ожидание сервера...";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Launcher2._0.Properties.Resources._4766504;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.lblVersion.Location = new System.Drawing.Point(835, 420);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(0, 11);
+            this.lblVersion.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 11F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Launcher2._0.Properties.Resources.Group_102;
+            this.ClientSize = new System.Drawing.Size(893, 465);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("RimWordFont", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rimworld Online Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label lblVersion;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,231 @@
+﻿#region Using
+using Microsoft.Win32;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using System.IO.Compression;
+using System.Diagnostics;
+using System.ComponentModel;
+using AutoUpdaterDotNET;
+#endregion
+
+namespace Launcher2._0
+{
+    public partial class Form1 : Form
+    {
+
+        const string url_updater = "http://109.248.11.225/launcher/AutoUpdateData.xml";
+        const string url_game = "http://109.248.11.225/download/main.zip";
+        const string url_vers = "http://109.248.11.225/download/v.txt";
+        const string url_conf = "http://109.248.11.225/download/Config.zip";
+        string config_path = (System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData) + @"\..\LocalLow\Ludeon Studios\RimWorld by Ludeon Studios\Config");
+        string path = @"C:\Program Files";
+        string version = "none";
+        string server_version = "none";
+        readonly string exe_name = "RimWorldWin64.exe";
+        string game_path = "none";
+
+        private bool dd;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }    
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblVersion.Text = Application.ProductVersion;
+
+            AutoUpdater.Start(url_updater);
+
+            Config_download();
+            label1.Hide();
+
+            //Кнопки ВК и Дискорд
+            button2.Text = "VK";
+            button2.Click += vk;
+            button3.Text = "Discord";
+            button3.Click += discrod;
+
+            //Инициализация регистра и создание дефолтного пути установки
+            dd = (Registry.CurrentUser.CreateSubKey("RimworldOnline").GetValue("path") == null) | ((string)Registry.CurrentUser.CreateSubKey("RimworldOnline").GetValue("path") == "");
+            if (dd)
+            {
+                game_path = path;
+                Registry.CurrentUser.CreateSubKey("RimworldOnline").SetValue("path", game_path);
+            }
+            else
+            {
+                game_path = (string)Registry.CurrentUser.CreateSubKey("RimworldOnline").GetValue("path");
+            }
+            version_controller(game_path);
+
+            //Инициализация кнопок и лейблов
+
+            if (File.Exists(game_path + @"/main.zip"))
+            {
+                File.Delete(game_path + @"/main.zip");
+            }            
+        }
+
+        private void version_controller(string path_1)
+        {
+            version = (string)Registry.CurrentUser.CreateSubKey("RimworldOnline").GetValue("version");
+
+            WebClient client_ver = new WebClient();
+            client_ver.DownloadFileAsync(new Uri(url_vers), path_1 + @"\" + "v.txt");
+            client_ver.DownloadFileCompleted += Client_ver_DownloadFileCompleted;
+        }
+
+        private void Client_ver_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            server_version = File.ReadAllText(game_path + @"\" + "v.txt");
+            if (version == null)
+            {
+                label2.Text = "Игра не установлена";
+            }
+            else
+            {
+                label2.Text = ("Установленная Версия Игры: " + version + @". Последняя версия: " + server_version);
+            }
+
+
+            if (version != server_version)
+            {
+                button1.Text = "Обновить";
+                button1.Click += GameUpdate;
+            }
+            else
+            {
+                button1.Text = "Играть";
+                button1.Click += Play;
+            }
+        }
+
+        private void GameUpdate(object sender, EventArgs e)
+        {
+            if ((!(File.Exists(game_path + @"\main"))))
+            {
+
+                WebClient client = new WebClient();
+                client.DownloadProgressChanged += Client_DownloadProgressChanged;
+                client.DownloadFileCompleted += Client_DownloadFileCompleted;
+                client.DownloadFileAsync(new Uri(url_game), (game_path + @"\" + "main.zip"));
+                button1.Enabled = false;
+                button1.BackColor = Color.Green;
+
+            }
+            else
+            {
+                button1.Text = "Играть";
+                button1.Click += Play;
+            }
+            Registry.CurrentUser.CreateSubKey("RimworldOnline").SetValue("version", server_version);
+        }
+        private void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Maximum = (int)e.TotalBytesToReceive;
+            progressBar1.Value = (int)e.BytesReceived;
+            label1.Show();
+            label1.Text = (e.ProgressPercentage.ToString() + "%/100%");
+        }
+        private void Client_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            button1.Enabled = true;
+            button1.BackColor = Color.DarkGoldenrod;
+            ZipFile.ExtractToDirectory((game_path + @"\main.zip"), game_path);
+            File.Delete(game_path + @"\main.zip");
+            button1.Text = "Играть";
+            button1.Click += Play;
+            Application.Restart();
+        }
+
+
+
+
+        private void Play(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start(game_path + @"\main\" + exe_name);
+                File.Delete(game_path + @"\v.txt");
+                if (File.Exists(game_path + @"\main.zip")) { File.Delete(game_path + @"\main.zip"); }
+            }
+
+            catch
+            {
+                MessageBox.Show("Ошибка запуска клиента. Перезапустите программу");
+                if (!((string)Registry.CurrentUser.OpenSubKey("RimworldOnline").GetValue("path") == ""))
+                {
+                    RegistryKey key = Registry.CurrentUser.CreateSubKey("RimworldOnline");
+                    key.SetValue("path", "");
+                    key.SetValue("version", "");
+                    key.Close();
+                }
+            }
+
+            finally
+            {
+
+                Registry.CurrentUser.Close();
+                this.Close();
+            }
+        }
+
+        private void vk(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://vk.com/rimworldline");
+        }
+
+        private void discrod(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://discord.gg/GCQsZKKkTx");
+        }
+
+
+
+        private void pictureBox1_Click(object sender, EventArgs e) //Выбор локации установки
+        {
+            folderBrowserDialog1.ShowDialog();
+            game_path = folderBrowserDialog1.SelectedPath;
+            Registry.CurrentUser.CreateSubKey("RimworldOnline").SetValue("path", game_path);
+            Application.Restart();
+        }
+
+        private void Config_download()
+        {
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(config_path);
+                foreach (FileInfo file in dirInfo.GetFiles())
+                {
+                    file.Delete();
+                }
+
+                WebClient client_conf = new WebClient();
+                client_conf.DownloadFileAsync(new Uri(url_conf), (config_path + @"\Config.zip"));
+                client_conf.DownloadFileCompleted += Client_conf_DownloadFileCompleted;
+            }
+            catch
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo((System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData) + @"\..\LocalLow\"));
+                dirInfo.CreateSubdirectory(@"Ludeon Studios\RimWorld by Ludeon Studios\Config");
+                WebClient client_conf = new WebClient();
+                client_conf.DownloadFileAsync(new Uri(url_conf), (config_path + @"\Config.zip"));
+                client_conf.DownloadFileCompleted += Client_conf_DownloadFileCompleted;
+            }
+
+        }
+
+        private void Client_conf_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            ZipFile.ExtractToDirectory((config_path + @"\Config.zip"), config_path);
+            File.Delete(config_path + @"\Config.zip");
+        }
+
+        
+
+    }
+}
